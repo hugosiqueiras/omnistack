84518c46b50996b96932b4416719a63fdad3a751
hugo <hugo.siqueira@outlook.com.br>
2020-03-29 01:37:26 -0300

    mobile app

---
+++ mobile/App.js
@@ -0,0 +1,13 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+//no html... div, span, p, header, footer
+
+import Routes from './src/routes';
+
+export default function App() {
+  return <Routes/>;
+}
